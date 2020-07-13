6aec1e70db45fdbd1dbc022b7c8d787e6bb8d8d6
Alexander <youdoit@mail.ru>
2020-07-14 05:53:14 +0600

    Still logging

--- taskmanager/Tasker/views.py
+++ taskmanager/Tasker/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from .models import Tasks, Users
 from .forms import UsersForm
 from django.contrib.auth import login, authenticate
@@ -6,24 +6,27 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
-
 def show(request):
-    tasks = Tasks.objects.filter(user_id=str(0))
+    tasks = Tasks.objects.filter(user_id=str( ))
     return render(request, 'Tasker/tasks.html', {'tasks':tasks})
+
 
 def finished(request):
     return render(request, 'Tasker/add-task.html')
+
 
 def index(request):
     if request.method=="POST":
         form = UsersForm(request.POST)
         if form.is_valid():
             form.save()
+            uss = len(list(Users.objects.values()))
             username = form.cleaned_data.get('login')
             raw_password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('main')
+            render(request,)
+            return 
     else:
         form = UsersForm()
         context = {
